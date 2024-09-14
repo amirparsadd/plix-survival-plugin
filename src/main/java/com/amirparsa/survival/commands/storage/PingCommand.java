212c54285ae1e3fef11327854864b6f3bea89de8
AmirparsaDD <Amirparsab9@gmail.com>
2024-09-14 15:44:31 +0330

    added support for checking other people's ping

--- src/main/java/com/amirparsa/survival/commands/storage/PingCommand.java
+++ src/main/java/com/amirparsa/survival/commands/storage/PingCommand.java
@@ -1,6 +1,7 @@
 package com.amirparsa.survival.commands.storage;
 
 import com.amirparsa.survival.commands.ConfigurableCommand;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -8,10 +9,24 @@ import org.bukkit.entity.Player;
 
 public class PingCommand extends ConfigurableCommand {
 
-    public boolean handle(CommandSender commandSender, Command command, String s, String[] strings) {
+    public boolean handle(CommandSender commandSender, Command command, String s, String[] args) {
         if(commandSender instanceof Player player){
-            player.sendMessage(ChatColor.GREEN + "Your Ping Is: " + ChatColor.DARK_GREEN + player.getPing() + "ms");
+            if(args.length != 1){
+                player.sendMessage(ChatColor.GREEN + "Your Ping Is: " + ChatColor.DARK_GREEN + player.getPing() + "ms");
+                return true;
+            }
+
+            Player selectedPlayer = Bukkit.getPlayer(args[0]);
+
+            if (selectedPlayer == null){
+                player.sendMessage(ChatColor.RED + "Unable to find specified player!");
+                return true;
+            }
+
+            player.sendMessage(ChatColor.GREEN + selectedPlayer.getName() + "'s Ping Is: " + ChatColor.DARK_GREEN + selectedPlayer.getPing() + "ms");
+            return true;
         }
+        commandSender.sendMessage("Unable To Execute Command From This Scope!");
         return true;
     }
 }
